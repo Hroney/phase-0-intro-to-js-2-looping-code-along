cb33136670b503b30541654f21e276acd03ebc60
Hayden Roney <roneyhayden@gmail.com>
2023-09-10 10:18:27 -0500

    lab finished

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(stringArray, eventName) {
+    const arrayToReturn = [];
+    for (let i = 0; i < stringArray.length; i++) {
+        arrayToReturn[i] = `Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return arrayToReturn;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
